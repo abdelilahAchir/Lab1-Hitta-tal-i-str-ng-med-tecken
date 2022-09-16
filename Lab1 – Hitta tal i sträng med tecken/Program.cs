ab8dcd7b22f23a313026e7c3729eee34601ba975
Abdelilah Achir <abdiachir@gmail.com>
2022-09-16 15:11:47 +0200

    Add README.md.

---
+++ Lab1 – Hitta tal i sträng med tecken/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Lab1 – Abdelilah Achir
+
+using Lab1___Hitta_tal_i_sträng_med_tecken;
+
+string inputString = Console.ReadLine();
+PrintSubstringsColored(inputString);
+
+void PrintSubstringsColored(string inputString)
+{
+    if (inputString.Length > 1)
+    {
+        Console.Clear();
+        substringIndexes[] subStrings = FindPositionAndLengh(inputString);
+        bool stringNotEmpty;
+        int beginingOfTheInputString = 0;
+        for (int x = 0; x < subStrings.Count(); x++)
+        {
+            stringNotEmpty = subStrings[x].length != 0;
+            if (stringNotEmpty)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(inputString.Substring(beginingOfTheInputString, subStrings[x].startIndex));
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(inputString.Substring(subStrings[x].startIndex, subStrings[x].length));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(inputString.Substring(subStrings[x].length + subStrings[x].startIndex));
+                Console.WriteLine();
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("The input was empty or entered just one character");
+    }
+    
+}
+
+
+substringIndexes[] FindPositionAndLengh(string inputString)
+{
+    int start;
+    int end;
+    int length;
+    bool isNotaNumber;
+    bool startAndEndAreTheSame;
+    substringIndexes[] subStrings = new substringIndexes[inputString.Length];
+    for (int i = 0; i < inputString.Length; i++)
+    {
+        for (int x = i + 1; x < inputString.Length; x++)
+        {
+            start = i;
+            end = x;
+            isNotaNumber = !Char.IsDigit(inputString[x]);
+            startAndEndAreTheSame = inputString[start] == inputString[end];
+            if (isNotaNumber)
+            {
+                break;
+            }
+            else if (startAndEndAreTheSame)
+            {
+                length = end - start + 1;
+                subStrings[i].startIndex = start;
+                subStrings[i].length = length;
+                break;
+            }
+        }
+    }
+    return subStrings;
+}
+
---
+++ Lab1 – Hitta tal i sträng med tecken/substringIndexes.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab1___Hitta_tal_i_sträng_med_tecken
+{
+    public struct substringIndexes
+    {
+        public int startIndex;
+        public int length;
+    }
+}
